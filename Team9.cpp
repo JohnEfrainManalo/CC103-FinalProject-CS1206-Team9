959f1472c66887d35b036a6903dafd33139db831
JohnEfrainManalo <mjohnefrain@gmail.com>
2026-04-04 13:49:40 +0800

    First Draft

---
+++ Team9.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std;
+
+int main(){
+    cout << "Draft by Team 9" << endl
+         << "Members: " << endl
+         << "Altha Jayne I. Jayawon" << endl
+         << "John Efrain V. Manalo" << endl
+         << "Leneth M. Mondragon" << endl;
+
+return 0;
+}
