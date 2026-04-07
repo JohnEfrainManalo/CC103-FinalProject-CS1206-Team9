858a7c3017bc2f54e528c6eebca3c159efc1387a
JohnEfrainManalo <mjohnefrain@gmail.com>
2026-04-07 13:56:57 +0800

    Drafting the functions

--- Team9.cpp
+++ Team9.cpp
@@ -1,12 +1,25 @@
 #include <iostream> 
 using namespace std;
 
+//enqueue
+//dequeue
+//peek
+
+
+
 int main(){
-    cout << "Draft by Team 9" << endl
+
+
+
+
+
+return 0;
+}
+
+/*
+  cout << "Draft by Team 9" << endl
          << "Members: " << endl
          << "Altha Jayne I. Jayawon" << endl
          << "John Efrain V. Manalo" << endl
          << "Leneth M. Mondragon" << endl;
-
-return 0;
-}
+*/
